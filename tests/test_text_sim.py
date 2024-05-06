b25eef4f2fd3195e2511edf4ff24fcc204b81505
xgao2 <gxiang1228@gmail.com>
2024-05-05 20:43:52 -0700

    unittest

--- tests/test_text_sim.py
+++ tests/test_text_sim.py
@@ -1,34 +1,38 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from text_sim import TextSimilarity
+import unittest
 
+class TestTextSimilarity(unittest.TestCase):
 
-def test():
-    """
-    in this example,
-    to human, `a` looks more similar to `b` than `c`
-    let's make sure the metric agrees with this.
-    
-    expected output:
-    
-    rouge1: score_ab = 0.5000, score_ac = 0.0000
-    rouge2: score_ab = 0.0000, score_ac = 0.0000
-    rougeL: score_ab = 0.5000, score_ac = 0.0000
-    sbert: score_ab = 0.7880, score_ac = 0.1007
-    bertscore: score_ab = 0.9551, score_ac = 0.8531
-    """
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName)
+        """
+        in this example,
+        to human, `a` looks more similar to `b` than `c`
+        let's make sure the metric agrees with this.
+        
+        expected output:
+        
+        rouge1: score_ab = 0.5000, score_ac = 0.0000
+        rouge2: score_ab = 0.0000, score_ac = 0.0000
+        rougeL: score_ab = 0.5000, score_ac = 0.0000
+        sbert: score_ab = 0.7880, score_ac = 0.1007
+        bertscore: score_ab = 0.9551, score_ac = 0.8531
+        """
 
-    text_sim = TextSimilarity()
-    a = 'this is one sentence.'
-    b = 'it is a sentence.'
-    c = 'how are you?'
+        self.text_sim = TextSimilarity()
+        self.a = 'this is one sentence.'
+        self.b = 'it is a sentence.'
+        self.c = 'how are you?'
 
-    for method in ['rouge1', 'rouge2', 'rougeL', 'sbert', 'bertscore']:
-        score_ab = text_sim.score(a, b, method=method)
-        score_ac = text_sim.score(a, c, method=method)
-        print('%s: score_ab = %.4f, score_ac = %.4f'%(method, score_ab, score_ac))
-        assert(score_ab >= score_ac)
+    def test(self):
+        for method in ['rouge1', 'rouge2', 'rougeL', 'sbert', 'bertscore']:
+            score_ab = self.text_sim.score(self.a, self.b, method=method)
+            score_ac = self.text_sim.score(self.a, self.c, method=method)
+            print('%s: score_ab = %.4f, score_ac = %.4f'%(method, score_ab, score_ac))
+            self.assertTrue(score_ab >= score_ac)
 
 
 if __name__ == '__main__':
-    test()
+    unittest.main()
